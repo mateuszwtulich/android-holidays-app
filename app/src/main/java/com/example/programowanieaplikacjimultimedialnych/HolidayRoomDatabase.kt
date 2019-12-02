705ea1c45fbc5c82f53c48c330c2d8c677b834a3
Mateusz Wtulich <mwtulich3@gmail.com>
2019-12-02 21:54:03 +0100

    RDB

---
+++ app/src/main/java/com/example/programowanieaplikacjimultimedialnych/HolidayRoomDatabase.kt
@@ -0,0 +1,39 @@
+package com.example.programowanieaplikacjimultimedialnych
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.programowanieaplikacjimultimedialnych.Model.Location
+import com.example.programowanieaplikacjimultimedialnych.Model.MultimediaPath
+import com.example.programowanieaplikacjimultimedialnych.Model.Post
+
+// Annotates class to be a Room Database with a table (entity) of the Word class
+@Database(entities = arrayOf(Post::class, MultimediaPath::class, Location::class), version = 1, exportSchema = false) //export Schema w normalnej apce inaczej
+public abstract class HolidayRoomDatabase : RoomDatabase() {
+
+    abstract fun holidayDao(): HolidayDao
+
+    companion object {
+        // Singleton prevents multiple instances of database opening at the
+        // same time.
+        @Volatile
+        private var INSTANCE: HolidayRoomDatabase? = null
+
+        fun getDatabase(context: Context): HolidayRoomDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    HolidayRoomDatabase::class.java,
+                    "holiday_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
