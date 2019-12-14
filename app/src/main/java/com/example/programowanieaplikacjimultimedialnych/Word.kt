2fed55756c04564af723a826bfa7d4c93d5f0ecb
Mateusz Wtulich <mwtulich3@gmail.com>
2019-12-14 20:30:23 +0100

    Fragments

--- app/src/main/java/com/example/programowanieaplikacjimultimedialnych/Word.kt
+++
@@ -1,9 +0,0 @@
-package com.example.programowanieaplikacjimultimedialnych
-
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-
-@Entity(tableName = "word_table")
-class Word(@PrimaryKey @ColumnInfo(name = "word") val word: String)
---
+++ app/src/main/java/com/example/programowanieaplikacjimultimedialnych/room_database/model/Post.kt
@@ -0,0 +1,13 @@
+package com.example.programowanieaplikacjimultimedialnych.room_database.model
+
+import androidx.room.*
+
+
+@Entity(tableName = "post_table")
+class Post(@PrimaryKey(autoGenerate = true) val id: Int,
+           val title : String,
+           val text : String,
+           val date : String,
+           val attitude  : Double,
+           val latitude : Double)
+
