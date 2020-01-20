b5843b5de0915e50a7b7e0d47eee16640fa7b19d
Lapierz <34073503+Lapierz@users.noreply.github.com>
2020-01-20 13:23:11 +0100

    xd

---
+++ app/src/androidTest/java/com/example/programowanieaplikacjimultimedialnych/repository/HolidayRepositoryTest.kt
@@ -0,0 +1,32 @@
+package com.example.programowanieaplikacjimultimedialnych.repository
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class HolidayRepositoryTest {
+
+    @Test
+    fun getPosts() {
+    }
+
+    @Test
+    fun getPost() {
+    }
+
+    @Test
+    fun insertPost() {
+    }
+
+    @Test
+    fun deletePost() {
+    }
+
+    @Test
+    fun update() {
+    }
+
+    @Test
+    fun deleteAllPost() {
+    }
+}
